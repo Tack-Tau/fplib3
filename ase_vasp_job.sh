9fba3857f8ef7e1c72594f09a06f24802c5a0ef4
Tack_Tau <taoshuo950111@gmail.com>
2022-04-04 15:44:33 -0400

    Combining vasp_calculator and fp_calculator in ase_test.py

---
+++ ase_vasp_job.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+#SBATCH --partition main
+#SBATCH --nodes=1
+#SBATCH --ntasks=16
+#SBATCH --mem-per-cpu=2GB
+#SBATCH --time=02:00:00
+
+module purge
+module use /projects/community/modulefiles
+module load intel/17.0.4 python/3.8.5-gc563
+ulimit -s unlimited
+ulimit -s
+export OMP_NUM_THREADS=1
+export VASP_SCRIPT="$PWD/ase_test.py"
+export VASP_PP_PATH="/home/lz432/apps/PBE54/"
+
+conda activate ase
+
+cp Li1.vasp POSCAR
+python3 ase_test.py
