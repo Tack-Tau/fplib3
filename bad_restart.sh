73af6511a2f23fc30b7fea0a595fd04d846b9eb3
Tack-Tau <taoshuo950111@gmail.com>
2022-09-22 14:52:45 -0400

    Add KPOINTS into rm list

--- bad_restart.sh
+++ bad_restart.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 for i in {1..200} ; do grep -l "Traceback" ${i}/slurm-* ; done > error_log
 awk -F '/' '{print $1 }' error_log > error_dir
-input="./error_dir" ; while IFS= read -r line ; do cd ./$line ; rm slurm-* POSCAR CONTCAR OUTCAR WAVECAR ; cd .. ; done < "$input"
+input="./error_dir" ; while IFS= read -r line ; do cd ./$line ; rm slurm-* KPOINTS POSCAR CONTCAR OUTCAR WAVECAR ; cd .. ; done < "$input"
 input="./error_dir" ; while IFS= read -r line ; do cp ../../struct/$line/BPOSCAR ./$line/POSCAR  ; done < "$input"
 input="./error_dir" ; while IFS= read -r line ; do cd ./$line ; sbatch ase_fp_job.sh ; cd .. ; done < "$input"
