16bfb15b05f5ebac2eede846197a979ad9aed60f
Tack_Tau <taoshuo950111@gmail.com>
2022-12-13 16:24:15 -0500

    Add and modify some utilities

---
+++ next_SCF.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+input="./redo_SCF_dir" ; while IFS= read -r line ; do cd fVfV/$line ; rm slurm-* opt.* KPOINTS POSCAR CONTCAR OUTCAR WAVECAR ; cd ../.. ; done < "$input"
+input="./redo_SCF_dir" ; while IFS= read -r line ; do cp fp/$line/opt.vasp fVfV/$line/POSCAR ; done < "$input"
+input="./redo_SCF_dir" ; while IFS= read -r line ; do cd fVfV/$line ; sbatch ase_fp_VASP_job.sh ; cd ../.. ; done < "$input"
