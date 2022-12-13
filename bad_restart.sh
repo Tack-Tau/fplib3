b3316700ccad6e05b9a64db6e3101fc39c7da22e
Tack_Tau <taoshuo950111@gmail.com>
2022-12-13 11:06:00 -0500

    Redirect stderr to null

--- bad_restart.sh
+++ bad_restart.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-for i in {1..200} ; do grep -l "Traceback" ${i}/slurm-* ; done > error_log
+for i in {1..200} ; do grep -l "Traceback" ${i}/slurm-* ; done 1> error_log 2> /dev/null
 awk -F '/' '{print $1 }' error_log > error_dir
 input="./error_dir" ; while IFS= read -r line ; do cd ./$line ; rm slurm-* KPOINTS POSCAR CONTCAR OUTCAR WAVECAR ; cd .. ; done < "$input"
 input="./error_dir" ; while IFS= read -r line ; do cp ../../struct/$line/BPOSCAR ./$line/POSCAR  ; done < "$input"
--- redo_calc.sh
+++ redo_calc.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-for i in {1..200} ; do grep -l "JOB\|CANCELLED" ${i}/slurm-* ; done > error_log
+for i in {1..200} ; do grep -l "JOB\|CANCELLED" ${i}/slurm-* ; done 1> error_log 2> /dev/null
 awk -F '/' '{print $1 }' error_log > error_dir
 input="./error_dir" ; while IFS= read -r line ; do cd ./$line ; rm slurm-* ; bash traj2optvasp.sh ; cd .. ; done < "$input"
 input="./error_dir" ; while IFS= read -r line ; do cd ./$line ; cp opt.vasp POSCAR ; sbatch ase_fp_job.sh ; cd .. ; done < "$input"
