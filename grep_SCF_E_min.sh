09764bec30b453a85595867051c41091b763eaae
Tack_Tau <taoshuo950111@gmail.com>
2022-12-14 16:28:55 -0500

    Add and modify some utilities

---
+++ grep_SCF_E_min.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+function exists_in_list() {
+  LIST=$1
+  DELIMITER=$2
+  VALUE=$3
+  echo $LIST | tr "$DELIMITER" '\n' | grep -F -q -x "$VALUE"
+}
+redo_dir="$(input="./redo_SCF_dir" ; while IFS= read -r line ; do printf "%s " $line ; done < "$input")"
+for i in {1..200} ; do if exists_in_list "$redo_dir" " " $i ; then grep "FIRE:" fV/${i}/slurm-* | tail -1 | awk '{print $4 }' ; else grep "FIRE:" fVfV/${i}/slurm-* | tail -1 | awk '{print $4 }' ; fi ; done > SCF_E_min_list.dat
