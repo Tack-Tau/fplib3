85598b297c8e017814d1d90911ff850ebd205af7
Tack_Tau <taoshuo950111@gmail.com>
2022-12-14 09:52:42 -0500

    Add and modify some utilities

--- grep_E_min.sh
+++ grep_E_min.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 for i in {1..200} ; do grep "BFGS:" ${i}/slurm-* | tail -1 | awk '{print $4 }' ; done > E_min_list.dat
+cat -n E_min_list.dat | sort -gk 2 > sorted_E_min_list.dat ; sed -i 's/*//g' sorted_E_min_list.dat
