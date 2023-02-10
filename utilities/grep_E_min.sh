02448929109b3a4bb2af885f35f3c1f24232412b
Tack_Tau <taoshuo950111@gmail.com>
2023-02-10 15:06:51 -0500

    Minor change to grep E min

--- utilities/grep_E_min.sh
+++ utilities/grep_E_min.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-for i in {1..200} ; do grep "BFGS:" ${i}/slurm-* | tail -1 | awk '{print $4 }' ; done > E_min_list.dat
-cat -n E_min_list.dat | sort -gk 2 > sorted_E_min_list.dat ; sed -i 's/*//g' sorted_E_min_list.dat
+for i in {1..200} ; do grep -A1 "Final energy per atom is" ${i}/slurm-* | tail -1 | awk '{print $1 }' ; done > E_min_list.dat
+sed -i 's/*//g' E_min_list.dat
+cat -n E_min_list.dat | sort -gk 2 > sorted_E_min_list.dat
--- utilities/grep_SCF_E_min.sh
+++ utilities/grep_SCF_E_min.sh
@@ -6,5 +6,6 @@ function exists_in_list() {
   echo $LIST | tr "$DELIMITER" '\n' | grep -F -q -x "$VALUE"
 }
 redo_dir="$(input="./redo_SCF_dir" ; while IFS= read -r line ; do printf "%s " $line ; done < "$input")"
-for i in {1..200} ; do if exists_in_list "$redo_dir" " " $i ; then grep "FIRE:" fVfV/${i}/slurm-* | tail -1 | awk '{print $4 }' ; else grep "FIRE:" fV/${i}/slurm-* | tail -1 | awk '{print $4 }' ; fi ; done > SCF_E_min_list.dat
-cat -n SCF_E_min_list.dat | sort -gk 2 > sorted_SCF_E_min_list.dat ; sed -i 's/*//g' sorted_SCF_E_min_list.dat
+for i in {1..200} ; do if exists_in_list "$redo_dir" " " $i ; then grep -A1 "Final energy per atom is" fVfV/${i}/slurm-* | tail -1 | awk '{print $1 }' ; else grep -A1 "Final energy per atom is" fV/${i}/slurm-* | tail -1 | awk '{print $1 }' ; fi ; done > SCF_E_min_list.dat
+sed -i 's/*//g' SCF_E_min_list.dat
+cat -n SCF_E_min_list.dat | sort -gk 2 > sorted_SCF_E_min_list.dat
